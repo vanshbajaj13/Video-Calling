ad5fb847684c0b0b8d13eb8589850fd6027a2849
vanshbajaj13 <vanshbajaj705@gmail.com>
2023-08-02 16:17:02 +0530

    change to port 8000

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const { Server } = require("socket.io");
 
-const io = new Server(5000, {
+const io = new Server(8000, {
   cors: true,
 });
 
 const emailToSocketIdMap = new Map();
 const socketidToEmailMap = new Map();
 
-console.log("started");
+// console.log("started");
 
 io.on("connection", (socket) => {
   // console.log(`Socket Connected`, socket.id);
