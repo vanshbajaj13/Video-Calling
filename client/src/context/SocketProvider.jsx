ad4a7bd4330d784ee376071524a45ebca72ea57e
vanshbajaj13 <vanshbajaj705@gmail.com>
2023-08-02 22:54:37 +0530

    pl..

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:5000"), []);
+  const socket = useMemo(() => io("http://localhost:8000"), []);
 
   return (
     <SocketContext.Provider value={socket}>
