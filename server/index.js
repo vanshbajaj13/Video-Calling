ad75a99a0512845b41ef8e50321794ff4d05572a
vanshbajaj13 <vanshbajaj705@gmail.com>
2023-08-02 17:27:28 +0530

    change path of build

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ app.use(express.json());
 __dirname = path.resolve();
 
 
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   // handle all routes other than defined by us above
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "..","client", "build", "index.html"));
   });
 
 
